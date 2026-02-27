c394533754b8ea9ec3118792ccd29de1a6d8e6b4
GQP <jonisson.ig@hotmail.com>
2026-02-27 17:58:36 -0300

    Configure Filament admin panel and project service structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,8 +17,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Jonisson',
+            'email' => 'jonisson@teste.com',
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
